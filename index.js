fa59498c89a289cd281885521deccea36926b79a
mattdesl <dave.des@gmail.com>
2014-10-11 20:56:12 -0400

    temp hack until i can work out a way around blugin plugin issue

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var escape = require('js-string-escape')
 
 module.exports = function (file, opts) {
     if (/\.json$/.test(file)) return through()
+    if (module.exports.__emitter) return module.exports.__emitter(file, opts)
+
     if (!opts) opts = {}
     var vars = opts.vars || {
         __filename: file,
         __dirname: path.dirname(file)
     }
-    
+
     var sm = staticModule(
         { 'datauri': urify },
         { vars: vars }
@@ -23,3 +25,6 @@ module.exports = function (file, opts) {
         return "'"+escape(data)+"'"
     }
 }
+
+//Currently a hack to get plugin architecture working
+module.exports.__emitter = null
